9be07f85e2c9e6ccbd5ca356e9747298676a95db
Jesus <jesus.huanachin>
2023-02-10 13:43:26 -0500

    #Feature - Remove old examples

--- lib/data/data_sources/remote/abstract/skeleton_data_source.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_base_rootstrap/core/result_type.dart';
-import 'package:flutter_base_rootstrap/utils/custom_exception.dart';
-
-abstract class SkeletonDataSource {
-  Future<ResultType<bool, CustomException>> checkServerStatus();
-}
--- lib/domain/repositories/skeleton_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_base_rootstrap/utils/custom_exception.dart';
-import 'package:flutter_base_rootstrap/core/result_type.dart';
-
-abstract class SkeletonRepository {
-  Future<ResultType<bool, CustomException>> checkServerStatus();
-}
