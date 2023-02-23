2d8299777f0a2ab8ee9edac5e72fa59993974a02
Amaury <ricardo901130@gmail.com>
2023-02-23 11:29:10 -0300

    add firebase config for each flavor

--- lib/main/env/main_qa.dart
+++ lib/main/env/main_qa.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_base_rootstrap/main/env/env_config.dart';
 import 'package:flutter_base_rootstrap/main/init.dart';
@@ -8,5 +9,21 @@ void main() async {
     flavor: Flavor.qa,
     values: FlavorValues(baseUrl: "https://demo_qa/web_api.json"),
   );
+  //Add your firebase configuration here
+  /*await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "your_key",
+      authDomain: "your_auth_domain.firebaseapp.com",
+      databaseURL: "https://your_db_url.firebaseio.com",
+      projectId: "your_project_id",
+      storageBucket: "your_storage_bucket.appspot.com",
+      // Add your messaging sender Id
+      messagingSenderId: "000000000",
+      // Add your App Id
+      appId: "----------",
+      // Add your measurementId
+      measurementId: "G-21AWEQTWR",
+    ),
+  );*/
   init();
 }
