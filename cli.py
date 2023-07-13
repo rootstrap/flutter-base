6b109daaf857ca215d10c53f1879178bed6e8d0d
Amaury <ricardo901130@gmail.com>
2023-07-13 12:42:34 -0300

    fix cli

--- cli.py
+++ cli.py
@@ -13,28 +13,18 @@ def add_module(module_name):
     # Create a temporary directory to clone the repository
     temp_dir = "/Users/Shared"
 
+    module_branch = f"module/module_name"
+
     # git_cmd = "git clone -b " + module_name + " --no-checkout " + GITHUB_REPO + " " + temp_dir
     # os.system(git_cmd)
+    current_dir = os.path.dirname(os.path.realpath(__file__))
 
     os.chdir(temp_dir)
-    os.system(f"git clone {GITHUB_REPO}")
-    os.system(f"git checkout {module_name}")
-    #os.system(f"cp {TARGET_FOLDER} {TARGET_DESTINATION}")
-
-    # Clone the repository using git
-    # git_clone_cmd = ["git", "clone -b", git_ref, temp_dir]
-    # subprocess.run(git_clone_cmd)
-
-    # git.Git(temp_dir).clone(GITHUB_REPO).branch(module_name)
-    # .execute(git_cmd)
-    # .clone(GITHUB_REPO).branch(module_name)
-
-    # Copy the module to the desired folder
-    module_folder = os.path.join(MODULES_FOLDER, module_name)
-    shutil.copytree(temp_dir + "/flutter-base/" + module_folder, module_folder)
-
+    os.system(f"git clone -b {module_branch} {GITHUB_REPO}")
     # Clean up the temporary directory
-    shutil.rmtree(temp_dir)
+    shutil.copytree(f"/Users/Shared/flutter-base/modules/{module_name}",
+                    f"{current_dir}/modules/{module_name}")
+    shutil.rmtree(f"{temp_dir}/flutter-base")
 
     print(f"Module '{module_name}' added successfully.")
 
