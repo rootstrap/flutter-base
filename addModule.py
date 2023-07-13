c3f4b83ccf13f4734b4ae9767e7cdde40e85bae3
Amaury <ricardo901130@gmail.com>
2023-07-13 13:34:34 -0300

    enhancement module script

--- addModule.py
+++ addModule.py
@@ -1,8 +1,5 @@
 import os
-import subprocess
-import tempfile
 import shutil
-import git
 
 # Constants
 MODULES_FOLDER = "modules"
@@ -13,17 +10,17 @@ def add_module(module_name):
     # Create a temporary directory to clone the repository
     temp_dir = "/Users/Shared"
 
-    module_branch = f"module/module_name"
+    module_branch = f"module/{module_name}"
 
-    # git_cmd = "git clone -b " + module_name + " --no-checkout " + GITHUB_REPO + " " + temp_dir
-    # os.system(git_cmd)
     current_dir = os.path.dirname(os.path.realpath(__file__))
 
     os.chdir(temp_dir)
     os.system(f"git clone -b {module_branch} {GITHUB_REPO}")
-    # Clean up the temporary directory
+
     shutil.copytree(f"/Users/Shared/flutter-base/modules/{module_name}",
                     f"{current_dir}/modules/{module_name}")
+
+    # Clean up the temporary directory
     shutil.rmtree(f"{temp_dir}/flutter-base")
 
     print(f"Module '{module_name}' added successfully.")
@@ -31,7 +28,7 @@ def add_module(module_name):
 
 # Main function
 def main():
-    module_name = input("Enter module name: ")
+    module_name = input("Add or Update module. \n Enter module name: ")
     add_module(module_name)
 
 
