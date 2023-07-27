a60396450e2781df2c89b543285d626bff5c17b1
Amaury <ricardo901130@gmail.com>
2023-07-27 09:44:40 -0300

    Improve responsive widget (#83)
    * improve responsive widget
    
    * add readme android configuration
    
    * add keystore config

--- modules/common/lib/init.dart
+++ modules/common/lib/init.dart
@@ -11,8 +11,11 @@ import 'package:common/devices/platform/concrete/_app_platform_impl.dart'
     if (dart.library.html) 'package:common/devices/platform/concrete/web/_app_platform_impl.dart';
 import 'package:common/devices/platform/concrete/_platform_info_impl.dart';
 
+late GetIt getIt;
+
 class CommonInit {
-  static Future<void> initialize(GetIt getIt) async {
+  static Future<void> initialize(GetIt getItInst) async {
+    getIt = getItInst;
     getIt.registerSingleton<AppPlatform>(AppPlatformImpl());
     getIt.registerLazySingleton<PlatformInfo>(() => PlatformInfoImpl(getIt()));
     getIt.registerLazySingleton<PermissionManager>(() {
