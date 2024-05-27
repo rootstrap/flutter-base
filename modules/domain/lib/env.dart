262bc82c58ab4c4cbf13020ea478671fa9dd4350
fabianvalencia <fabian.valencia@rootstrap.com>
2024-05-27 16:33:52 -0500

    Upgrade flutter changes needed and removed dart code metric

--- modules/domain/lib/env.dart
+++ modules/domain/lib/env.dart
@@ -1,3 +1,3 @@
 class EnvConfig {
-  static const apiUrl = "http://api.com"; //String.fromEnvironment('API_URL', defaultValue: "http://movies.com");
+  static const apiUrl = "https://dummyjson.com"; //String.fromEnvironment('API_URL', defaultValue: "http://movies.com");
 }
