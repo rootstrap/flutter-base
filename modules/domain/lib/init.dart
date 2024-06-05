ffa9681ef604665291acde2197d2ac3ee4abe00a
aguskoll <aguskoll@gmail.com>
2024-06-05 16:14:43 -0300

    removed commented code

--- modules/domain/lib/init.dart
+++ modules/domain/lib/init.dart
@@ -1,13 +1,10 @@
 import 'package:domain/bloc/app/app_cubit.dart';
 import 'package:domain/bloc/auth/auth_cubit.dart';
-//import 'package:data/init.dart';
 import 'package:domain/services/AuthService.dart';
 import 'package:get_it/get_it.dart';
 
 class DomainInit {
   static Future<void> initialize(GetIt getIt) async {
-    //await DataInit.initialize(getIt);
-
     //Cubits
     getIt.registerSingleton(AppCubit(getIt()));
     getIt.registerSingleton(SessionCubit());
